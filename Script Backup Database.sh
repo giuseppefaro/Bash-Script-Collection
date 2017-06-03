84b8a0c8912b68250c2ad561640545de2c43d7be
Giuseppe Faro <giuseppe.faro88@gmail.com>
2017-06-03 22:14:41 +0100

    Autocleaning

--- Script Backup Database.sh
+++ Script Backup Database.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #################################################################################
-# This script will backup all database listed in the variable "mydatabase"	#
-# and will save them in the folder specify below				#
-#									   	#
-# NOTE: this script work only with Mysql		                    	#
-#									   	#
-# Created By Giuseppe Faro 						     	#
+# This script will backup all database listed in the variable "mydatabase"  	#
+# and will save them in the folder specify below				                #
+#									                                          	#
+# NOTE: this script work only with Mysql		                               	#
+#				                                        					   	#
+# Created By Giuseppe Faro 		                        				     	#
 #################################################################################
 
 #################################################################################
-# Add other database respecting the same format	in the example			#
-# example add 'DatabaseName' below the other DB  				#
+# Add other database respecting the same format	in the example	           		#
+# example add 'DatabaseName' below the other DB  			                	#
 #################################################################################
 
 mydatabase=(
@@ -19,14 +19,17 @@ mydatabase=(
   )
   
 ################################# Settings ######################################
-# Change myuser if you dont want that root is the owner of the backup file	#
+# Change myuser if you dont want that root is the owner of the backup file  	#
 #################################################################################
 
-mydbuser="" 				# User on the Database
-mypass=""          			# password for the Database
-myuser=""				# Owner of backup files after the Chown	 
-backupfolder=" /var/backup "		# Folder for the DB Backup files ( You have to create the backup
-file_perm="440"				# File Permissions ( default 440 - read-read-none)
+mydbuser="" 				    # User on the Database
+mypass=""          		    	# password for the Database
+myuser=""				        # Owner of backup files after the Chown
+backupfolder="/var/backup "		# Folder for the DB Backup files ( You have to create the backup
+file_perm="440"				    # File Permissions ( default 440 - read-read-none)
+
+## this variable need to be implemented
+autoclean="0"                   # add the number of day to auto-delete the files, Set 0 to disable
 
 
 
@@ -49,11 +52,25 @@ done
 chown $myuser "$logfile"
 echo "file permission changed to "$myuser >> "$logfile"
 echo "Removing file older than 30 days" >> "$logfile"
-find "$backupfolder" -name $mydatabase"_*"  -mtime +30 >> "$logfile"
-find "$backupfolder" -name $mydatabase"_*"  -mtime +30 -exec rm {} \;
+
+
+# Auto-remove old Backup File
+
+if [ $autoclean = "0" ] ;
+then
+    echo The autoclean is disable
+else
+    find "$backupfolder" -name $mydatabase"_*"  -mtime +$autoclean >> "$logfile"
+    find "$backupfolder" -name $mydatabase"_*"  -mtime +$autoclean -exec rm {} \;
+fi
+
+
 echo "Backup completed at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
 echo "*****************" >> "$logfile"
 
 exit 0
+
+
+
 
 
