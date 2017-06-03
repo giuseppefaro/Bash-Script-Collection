e38ff6ef4b6132179bf5512245ede785280ecc2b
Giuseppe Faro <giuseppe.faro88@gmail.com>
2017-06-03 22:05:39 +0100

    Autocleaning

--- Script Backup Database.sh
+++ Script Backup Database.sh
@@ -28,6 +28,9 @@ myuser=""				        # Owner of backup files after the Chown
 backupfolder="/var/backup "		# Folder for the DB Backup files ( You have to create the backup
 file_perm="440"				    # File Permissions ( default 440 - read-read-none)
 
+## this variable need to be implemented
+autoclean="0"                   # add the number of day to auto-delete the files, Set 0 to disable
+
 
 
 #################################### Script #####################################
@@ -50,15 +53,18 @@ chown $myuser "$logfile"
 echo "file permission changed to "$myuser >> "$logfile"
 echo "Removing file older than 30 days" >> "$logfile"
 
-# if
-#
-#
-#
-#
-#
 
-find "$backupfolder" -name $mydatabase"_*"  -mtime +30 >> "$logfile"
-find "$backupfolder" -name $mydatabase"_*"  -mtime +30 -exec rm {} \;
+# Auto-remove old Backup File
+
+if [ $autoclean = "0" ] ;
+then
+    echo The autoclean is disable
+else
+    find "$backupfolder" -name $mydatabase"_*"  -mtime +$autoclean >> "$logfile"
+    find "$backupfolder" -name $mydatabase"_*"  -mtime +$autoclean -exec rm {} \;
+fi
+
+
 echo "Backup completed at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
 echo "*****************" >> "$logfile"
 
